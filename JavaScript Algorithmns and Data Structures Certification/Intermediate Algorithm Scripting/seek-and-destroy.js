6ed2e9e81bfe247bfde8c6e9ed9e056f791d1d9f
yenapaek <53715109+yenapaek@users.noreply.github.com>
2020-10-14 14:02:12 -0500

    Update seek-and-destroy.js

--- JavaScript Algorithmns and Data Structures Certification/Intermediate Algorithm Scripting/seek-and-destroy.js
+++ JavaScript Algorithmns and Data Structures Certification/Intermediate Algorithm Scripting/seek-and-destroy.js
@@ -1,13 +1,15 @@
 /* Intermediate Algorithm Scripting: Seek and Destroy */
 
-/* Problem:
- * You will be provided with an initial array (the first argument in the destroyer function),
-   followed by one or more arguments.
- * Remove all elements from the initial array that are of the same value as these arguments.
- * Note - You have to use the arguments object. */
+/*
+Problem: You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
+Remove all elements from the initial array that are of the same value as these arguments.
+
+Note - You have to use the arguments object.
+*/
 
 
 /* Solution */
+
 function destroyer(arr, ...args) {
 
     return arr.filter(function(el) {
